c551f676e936a477c775c8450f19ec6a359a518f
r_volchkov <r.volchkov@lookatme.ru>
2017-09-27 20:17:24 +0300

    v2

--- application/assets/javascript/application.js
+++ application/assets/javascript/application.js
@@ -50,7 +50,66 @@
         }
     });
     
-    app.controller('indexCtrl', function ($scope, $rootScope, $state, $timeout) {
+    app.controller('indexCtrl', function ($scope, $rootScope, $state, $timeout, $interval) {
+
+        $scope.toolsText = ['tools', '.', 'push', '(', ')', ';'];
+        $scope.toolsQuery = ['javascript', 'angular', 'css3', 'html5'];
+        $scope.usedTools = [];
+        var len = 0;
+        $(document).ready(function() {
+            setTimeout(function() {
+
+                textAction();
+
+                setInterval(function() {
+                    textAction();
+                }, 7000);
+
+            }, 2000);
+        })
+
+        getRandomInt = function(min, max) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+        textAction = function() {
+            //$('.tools-wrapper').removeClass('shown');
+            $('.tools').removeClass('hidden');
+            $('.tools').html('');
+            console.log(len)
+            if (len === $scope.toolsQuery.length) {
+                len = 0;
+                $scope.usedTools = [];
+            }
+            var ite = 0;
+            var intervalTools = setInterval(function() {
+                if (ite === 3) {
+                    clearInterval(intervalTools);
+                    $timeout(function() {
+                        $('.tools').html($('.tools').html() + $scope.toolsQuery[len]);
+                        $scope.usedTools.push($scope.toolsQuery[len]);
+                        len++;
+                    }, 200);
+                    $timeout(function() {
+                        $('.tools').html($('.tools').html() + $scope.toolsText[4]);
+                    }, 400);
+                    $timeout(function() {
+                        $('.tools').html($('.tools').html() + $scope.toolsText[5]);
+                    }, 600);
+                    $timeout(function() {
+                        $('.tools').addClass('hidden');
+                        $timeout(function() {
+                            $('.tools-wrapper').addClass('shown');
+                        }, 1000);
+                        $timeout(function() {
+                            $('.tools-wrapper').removeClass('shown');
+                        }, 3750);
+                    }, 1500);
+                }
+                $('.tools').html($('.tools').html() + $scope.toolsText[ite])
+                ite++;
+            }, 200)
+        };
+
         $rootScope.$broadcast('indexAllSet');
     })
 
